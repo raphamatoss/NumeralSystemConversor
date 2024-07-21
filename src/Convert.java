8b39fec3ecafb0e99836f75aebbd7e4fb90c0fab
Raphael Matos <raphaelmsgoncalves@gmail.com>
2024-07-21 12:21:16 -0300

    Conversion functions are now within a static class "Convert", there´s also small changes in how the functions return its values, it now returns the pure value of the conversion

---
+++ src/Convert.java
@@ -0,0 +1,56 @@
+public class Convert {
+    static String decimalToBinary(int decimal) {
+        StringBuilder sb = new StringBuilder();
+
+        if (decimal == 0 || decimal == 1) {
+            sb.append(decimal);
+            return sb.toString();
+        }
+        else {
+            do {
+                sb.insert(0, decimal%2);
+                decimal = decimal/2;
+            } while(decimal != 0);
+
+            return sb.toString();
+        }
+    }
+
+    static String decimalToHex(int decimal) {
+        StringBuilder sb = new StringBuilder();
+
+        /* IF decimal < 10 => the number stays the same
+         * ELSE
+         *     aux = decimal%16; ----> which gives us a number between 0 and 15
+         *     IF aux < 10 => the number stays the same
+         *     ELSE
+         *         aux2 = aux - 10; ----> which gives a number between 0 and 5
+         *
+         *         Since we know the number 10 equals to "A" in Hex, now we just need to get the
+         *         value of "A" in ASCII and sum it to aux2:
+         *           It gives us a range of [65, 70], which represents A,B,C,D,E,F in ASCII, in this order.
+         *         Then we convert the ASCII value into the Char it represents.
+         * */
+
+        if (decimal < 10) {
+            sb.append(decimal);
+            return sb.toString();
+        }
+        else {
+            do {
+                int aux = decimal%16;
+                if (aux < 10) {
+                    sb.insert(0, aux);
+                }
+                else {
+                    int aux2 = aux - 10;
+                    char ascii = (char) (65+aux2);
+                    sb.insert(0, ascii);
+                }
+                decimal = decimal/16;
+            } while(decimal != 0);
+            return sb.toString();
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -20,15 +19,20 @@ public class Main {
         int option = sc.nextInt();
 
         do {
+            int decimal;
             switch (option) {
                 case (0):
                     System.out.println("Finishing the program...");
                     return;
                 case (1):
-                    System.out.println(decimalToBinary());
+                    System.out.print("Insert the decimal value: ");
+                    decimal = sc.nextInt();
+                    System.out.println("Resultado: " + Convert.decimalToBinary(decimal));
                     return;
                 case (2):
-                    System.out.println(decimalToHex());
+                    System.out.print("Insert the decimal value: ");
+                    decimal = sc.nextInt();
+                    System.out.println("Resultado: " + Convert.decimalToHex(decimal));
                     return;
                 case (3):
                     return;
@@ -44,65 +48,5 @@ public class Main {
                     break;
             }
         } while (option != 0);
-    }
-
-    static String decimalToBinary() {
-        System.out.print("Insert the decimal value: ");
-        int decimal = sc.nextInt();
-
-        StringBuilder sb = new StringBuilder();
-
-        if (decimal == 0 || decimal == 1) {
-            sb.append(decimal);
-            return "Resultado: " + sb.toString();
-        }
-        else {
-            do {
-                sb.insert(0, decimal%2);
-                decimal = decimal/2;
-            } while(decimal != 0);
-
-            return "Resultado: " + sb.toString();
-        }
-    }
-
-    static String decimalToHex() {
-        System.out.print("Insert the decimal value: ");
-        int decimal = sc.nextInt();
-
-        StringBuilder sb = new StringBuilder();
-
-        /* IF decimal < 10 => the number stays the same
-        * ELSE
-        *     aux = decimal%16; ----> which gives us a number between 0 and 15
-        *     IF aux < 10 => the number stays the same
-        *     ELSE
-        *         aux2 = aux - 10; ----> which gives a number between 0 and 5
-        *
-        *         Since we know the number 10 equals to "A" in Hex, now we just need to get the
-        *         value of "A" in ASCII and sum it to aux2:
-        *           It gives us a range of [65, 70], which represents A,B,C,D,E,F in ASCII, in this order.
-        *         Then we convert the ASCII value into the Char it represents.
-        * */
-
-        if (decimal < 10) {
-            sb.append(decimal);
-            return "Result: " + sb.toString();
-        }
-        else {
-            do {
-                int aux = decimal%16;
-                if (aux < 10) {
-                    sb.insert(0, aux);
-                }
-                else {
-                    int aux2 = aux - 10;
-                    char ascii = (char) (65+aux2);
-                    sb.insert(0, ascii);
-                }
-                decimal = decimal/16;
-            } while(decimal != 0);
-            return "Result: " + sb.toString();
-        }
     }
 }
